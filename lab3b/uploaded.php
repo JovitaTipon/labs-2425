3526c66b9e9198272a6f64798ff6ff99b4cb3cb8
JovitaTipon <tiponjovita@gmail.com>
2024-09-28 02:00:07 +0800

    changes

--- lab3b/uploaded.php
+++ lab3b/uploaded.php
@@ -3,15 +3,12 @@ $upload_directory = getcwd() . '/uploads/';
 $uploaded_image_file = $upload_directory . basename($_FILES['image_file']['name']);
 $temporary_file = $_FILES['image_file']['tmp_name'];
 
-// Check if the uploads directory exists, if not, create it
 if (!is_dir($upload_directory)) {
-    mkdir($upload_directory, 0777, true); // Create directory with proper permissions
+    mkdir($upload_directory, 0777, true);
 }
 
-// Allowed image MIME types
 $allowed_image_types = ['image/jpeg', 'image/png', 'image/gif'];
 
-// Check if the file is a valid image
 if (in_array($_FILES['image_file']['type'], $allowed_image_types)) {
     if (move_uploaded_file($temporary_file, $uploaded_image_file)) {
         echo "Image uploaded successfully!";
