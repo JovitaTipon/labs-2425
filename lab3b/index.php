8532c6ffba6ad60af2c79d7efd3e3fb7d9c9af3b
JovitaTipon <tiponjovita@gmail.com>
2024-09-28 00:00:17 +0800

    pdf upload

--- lab3b/index.php
+++ lab3b/index.php
@@ -20,27 +20,29 @@
 <div class="row--50-50 grid-demo">
   <div class="col">
     <h4>File Upload</h4>
-
-    <form>
+    
+    <form method="POST" action="uploaded.php" enctype="multipart/form-data">
         <div class="p-card">
-            <h3>Text File</h3>
+            <h3>PDF File</h3>
             <p class="p-card__content">
-            <input type="file" name="text_file" accept=".txt" />
+                <!-- Use $_FILES['pdf_file'] to handle file upload -->
+                <input type="file" name="pdf_file" accept=".pdf" />
             </p>
         </div>
 
         <div>
-            <button>
+            <button type="submit">
                 Upload
             </button>
         </div>
     </form>
-    </div>
+
+  </div>
+  
   <div class="col">
-  <img class="p-logo-section__logo" src="https://www.auf.edu.ph/home/images/mascot/CCS.png" alt="College of Computing Studies">
+    <img class="p-logo-section__logo" src="https://www.auf.edu.ph/home/images/mascot/CCS.png" alt="College of Computing Studies">
   </div>
 </div>
 
 </body>
 </html>
-
--- lab3b/uploaded.php
+++ lab3b/uploaded.php
@@ -1,22 +1,17 @@
 <?php
-
 $upload_directory = getcwd() . '/uploads/';
-$relative_path = '/uploads/';
+$uploaded_pdf_file = $upload_directory . basename($_FILES['pdf_file']['name']);
+$temporary_file = $_FILES['pdf_file']['tmp_name'];
 
-// Handle Text File
-$uploaded_text_file = $upload_directory . basename($_FILES['text_file']['name']);
-$temporary_file = $_FILES['text_file']['tmp_name'];
-
-if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
-    $text_file_content = file_get_contents($uploaded_text_file, 'r');
-    ?>
-    <textarea cols="70" rows="30"><?php echo $text_file_content; ?></textarea>
-    <?php
+if ($_FILES['pdf_file']['type'] == 'application/pdf') {
+    if (move_uploaded_file($temporary_file, $uploaded_pdf_file)) {
+        echo "PDF uploaded successfully!";
+        echo "<a href='/uploads/" . basename($_FILES['pdf_file']['name']) . "'>View PDF</a>";
+    } else {
+        echo "Failed to upload PDF file.";
+    }
 } else {
-    echo 'Failed to upload file';
+    echo "Please upload a valid PDF file.";
 }
 
-
-echo '<pre>';
-var_dump($_FILES);
 exit;
