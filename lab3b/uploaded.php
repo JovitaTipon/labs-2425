18f54d9ae6a374679a5c384ab1eeecc5be076bd0
JovitaTipon <tiponjovita@gmail.com>
2024-09-28 00:10:24 +0800

    mp3 file

--- lab3b/uploaded.php
+++ lab3b/uploaded.php
@@ -1,22 +1,22 @@
 <?php
-
 $upload_directory = getcwd() . '/uploads/';
-$relative_path = '/uploads/';
+$uploaded_audio_file = $upload_directory . basename($_FILES['audio_file']['name']);
+$temporary_file = $_FILES['audio_file']['tmp_name'];
 
-// Handle Text File
-$uploaded_text_file = $upload_directory . basename($_FILES['text_file']['name']);
-$temporary_file = $_FILES['text_file']['tmp_name'];
+if (!is_dir($upload_directory)) {
+    mkdir($upload_directory, 0777, true); // Create directory with permissions
+}
 
-if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
-    $text_file_content = file_get_contents($uploaded_text_file, 'r');
-    ?>
-    <textarea cols="70" rows="30"><?php echo $text_file_content; ?></textarea>
-    <?php
+if ($_FILES['audio_file']['type'] == 'audio/mpeg' && pathinfo($_FILES['audio_file']['name'], PATHINFO_EXTENSION) == 'mp3') {
+    if (move_uploaded_file($temporary_file, $uploaded_audio_file)) {
+        echo "MP3 uploaded successfully!";
+        echo "<a href='/uploads/" . basename($_FILES['audio_file']['name']) . "'>Listen to MP3</a>";
+    } else {
+        echo "Failed to upload MP3 file.";
+    }
 } else {
-    echo 'Failed to upload file';
+    echo "Please upload a valid MP3 file.";
 }
 
 
-echo '<pre>';
-var_dump($_FILES);
 exit;
